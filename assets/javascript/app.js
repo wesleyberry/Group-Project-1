d2910db3437d7b453ac20ff84bff0f745574c2ad
Katie <47231728+hootpoot@users.noreply.github.com>
2019-03-20 10:32:40 -0500

    improved search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,66 @@
 $(document).ready(function () {
 
-    function makeDrinkCards(drinkResults) {
-                
+    function makeDrinkCards(drinkResults) {   
         for (var i = 0; i < drinkResults.length; i++) {
+
+            var drink = drinkResults[i];
             var drinkCard = $("<div>");
-            var drinkName = $("<h3>").text(drinkResults[i].strDrink);
-            var drinkImg = drinkResults[i].strDrinkThumb;
-            
+            drinkCard.addClass("drinkCard");
+            $(".row.forDivThree").append(drinkCard);
+            var drinkName = $("<h3>").text(drink.strDrink);
             drinkCard.append(drinkName);
+            var drinkImg = $("<img>").attr("src", drink.strDrinkThumb);
             drinkCard.append(drinkImg);
-            $(".grid-container").append(drinkCard);
-            
+
+            for (var ingIndex = 1; ingIndex <= 15; ingIndex++) {
+                var ingredientProperty = "strIngredient" + ingIndex;
+                var ingredient = drink[ingredientProperty];
+                var measureProperty = "strMeasure" + ingIndex;
+                var measure = drink[measureProperty];
+
+                if (ingredient != "" && ingredient != null) {
+                    var item = $("<li>");
+                    item.text(measure + " " + ingredient);
+                    drinkCard.append(item);
+                }
+
+            }
+
+            var instructions = $("<p>");
+            var instructionsId = "inst" + drink.idDrink;
+            instructions.attr('id', instructionsId);
+            instructions.text(drink.strInstructions);
+            instructions.css("display", "none");
+            instructions.appendTo(drinkCard);
+
+            var expandButton = $("<button>");
+            expandButton.addClass("expand");
+            expandButton.text("Expand");
+            expandButton.attr('data-id', instructionsId);
+            expandButton.attr('data-expand', 'expand');
+            drinkCard.append(expandButton);
+
+            $(".expand").on("click", function (event) {
+                var paragraphId= "#" + $(this).attr("data-id");
+                if ( $(this).attr("data-expand") === "Expand") {
+                    $(paragraphId).css("display", "inline");
+                    $(this).text("collapse");
+                    $(this).attr("data-expand", "collapse");
+                } else {
+                    $(paragraphId).css('display', 'none');
+                    $(this).text("Expand");
+                    $(this).attr("data-expand", "Expand");
+                }
+            })
+
         }
     }
 
+
+
     $("#buttonSearchDrink").on("click", function (event) {
         event.preventDefault();
-        $(".grid-container").empty();
+        $(".row.forDivThree").empty();
         var drinkQuery = $("#search-drink").val().trim();
         var queryDrinkURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkQuery;
         $.ajax({
@@ -25,15 +69,16 @@ $(document).ready(function () {
             })
             .then(function (response) {
                 var drinkResults = response;
-                console.log(drinkResults);
                 makeDrinkCards(drinkResults.drinks);
             });
 
-        });
+
+
+    });
 
     $("#buttonSearchIngredient").on("click", function (event) {
         event.preventDefault();
-        $(".grid-container").empty();
+        $(".row.forDivThree").empty();
         var ingredientQuery = $("#search-ingredient").val().trim();
         var queryIngredientURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + ingredientQuery;
         $.ajax({
@@ -41,9 +86,8 @@ $(document).ready(function () {
                 method: "GET"
             })
             .then(function (response) {
-                var ingredientResults = response;
-                console.log(ingredientResults);
-
+                var drinkResults = response;
+                makeDrinkCards(drinkResults.drinks);
             })
 
     });
